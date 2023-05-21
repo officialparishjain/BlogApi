b5a65a2d6b49655192662adfaba93b6fc4ff653c
Parish Jain <officialparishjain@gmail.com>
2023-05-21 21:13:48 +0530

    done

--- src/main/java/com/parishjain/BlogginApi/controller/UserController.java
+++ src/main/java/com/parishjain/BlogginApi/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/blog")
+@RequestMapping(value = "/user")
 public class UserController {
 
     @Autowired
